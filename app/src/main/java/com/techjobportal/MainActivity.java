e197951ae3f2967cbcf7c5687227e17fd78077c3
AbdCodes <abdullah.miyan9848@gmail.com>
2019-05-20 17:22:09 +0545

    Splash Screen Initiated

--- app/src/main/java/com/techjobportal/MainActivity.java
+++ app/src/main/java/com/techjobportal/MainActivity.java
@@ -2,12 +2,24 @@ package com.techjobportal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static android.view.WindowManager.*;
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(FLAG_FULLSCREEN,
+                FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
     }
 }
